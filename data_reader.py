f121455d37d6e33a79d8eadc7deb010277991b5e
Jakob Viking <jakob.viking@outlook.com>
2021-03-05 10:11:38 +0100

    Data_reader.py records data from server

--- data_reader.py
+++ data_reader.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from datetime import datetime
 
 import csv
 import aiofiles
@@ -14,20 +15,24 @@ async def main():
     # TCP connection own server
     url = 'opc.tcp://0.0.0.0:4840/freeopcua/server/'
     file_name = 'dataset.csv'
+    # Prepare csv file
+    async with aiofiles.open(file_name, mode="w", encoding="utf-8", newline="") as afp:
+        writer = AsyncDictWriter(afp, ["time", "value"], restval="NULL", quoting=csv.QUOTE_ALL)
+        await writer.writeheader()
+
     async with Client(url=url) as client:
         while True:
             # Do stuff
-
             # Get  nodes
             # TODO: Get info from all nodes
             node = client.get_node("ns=2;i=2")
             value = await node.read_value()
-            
-            # Prepare csv file
-            async with aiofiles.open(file_name, mode="w", encoding="utf-8", newline="") as afp:
+            async with aiofiles.open(file_name, mode="a", encoding="utf-8", newline="") as afp:
                 writer = AsyncDictWriter(afp, ["time", "value"], restval="NULL", quoting=csv.QUOTE_ALL)
-                await writer.writeheader()
-                await writer.writerow({"time": 111, "value": value})
+                await writer.writerow({"time": datetime.timestamp(datetime.now()), "value": value})
+            
+            await asyncio.sleep(1)
+            
                 
 
 if __name__ == '__main__':
