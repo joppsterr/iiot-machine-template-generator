4e18312dd165b7de3a88de339e0d793907e9740a
Jakob Viking <jakob.viking@outlook.com>
2021-03-09 15:55:50 +0100

    Separated plotting and clustering

--- clusterer.py
+++ clusterer.py
@@ -4,14 +4,13 @@ from sklearn.preprocessing import StandardScaler
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 def main():
 
-    dataset = pd.read_csv("Plant_1_Generation_Data.csv")
+    dataset = pd.read_csv("gas_sensor_array.csv")
+    print(dataset.dtypes)
 
-    subset = dataset.select_dtypes(exclude=["object", "int64"])
+    subset = dataset
     labels_true = subset.columns
 
     X = StandardScaler().fit_transform(subset)
@@ -26,32 +25,12 @@ def main():
 
     print('Estimated number of clusters: %d' % n_clusters_)
     print('Estimated number of noise points: %d' % n_noise_)
-    #print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    #print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    #print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    #print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
-    #print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
+    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+    print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
+    print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
     print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
-
-    unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each)
-        for each in np.linspace(0, 1, len(unique_labels))]
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = [0, 0, 0, 1]
-
-        class_member_mask = (labels == k)
-
-        xy = X[class_member_mask & core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
-
-        xy = X[class_member_mask & ~core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
-
-    plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.show(True)
-    
 
 if __name__ == '__main__':
     main()
---
+++ result_plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_result(X, labels, core_samples_mask, n_clusters_):
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+        for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show(True)
