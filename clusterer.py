aa77299bf841bbc5d02af72dabd2022c52653316
Jakob Viking <jakob.viking@outlook.com>
2021-03-09 11:04:48 +0100

    Added more functionallity to clusterer and anylzer

--- clusterer.py
+++ clusterer.py
@@ -1,11 +1,57 @@
 from sklearn import cluster as cl
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
 
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
-    dbscan = cl.DBSCAN()
 
- 
+def main():
+
+    dataset = pd.read_csv("Plant_1_Generation_Data.csv")
+
+    subset = dataset.select_dtypes(exclude=["object", "int64"])
+    labels_true = subset.columns
+
+    X = StandardScaler().fit_transform(subset)
+
+    db = cl.DBSCAN(eps=0.3, min_samples=10).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print('Estimated number of noise points: %d' % n_noise_)
+    #print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+    #print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+    #print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+    #print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
+    #print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
+    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+        for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show(True)
+    
 
 if __name__ == '__main__':
     main()
---
+++ stat_analyzer.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def main():
+    # Analyze statistical properties of the data
+    sensor_data = [0,0,0,0,0,0,0,0,0,0,0]
+
+if __name__ == '__main__':
+    main()
