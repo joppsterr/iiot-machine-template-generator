eb8a3eca39f1e358e62f5f85070792025131e8a1
Jakob Viking <jakob.viking@outlook.com>
2021-03-11 16:11:44 +0100

    Added PCA dimension reduction to clustering

--- clusterer.py
+++ clusterer.py
@@ -1,6 +1,7 @@
 from sklearn import cluster as cl
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 import pandas as pd
 import numpy as np
@@ -16,19 +17,19 @@ def main():
     # subset = dataset[["Humidity","Heater voltage"]]
 
     # Reduce rows from 300 000 to specified n
-    small_subset = subset.sample(n=10000)
-    labels_true = subset.columns
+    small_subset = subset.sample(n=50000)
 
     # Transform data
-    X = StandardScaler().fit_transform(small_subset)
+    scaled_subset = StandardScaler().fit_transform(small_subset)
+
+    # Reduce dimension with PCA
+    pca = PCA(n_components=2)
+    pca_subset = pca.fit_transform(scaled_subset)
 
     # Run DBSCAN
-    db = cl.DBSCAN(min_samples=3).fit(X)
+    db = cl.DBSCAN(min_samples=3).fit(pca_subset)
 
-    ################################################################
     # Start of results
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
 
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
@@ -36,12 +37,7 @@ def main():
 
     print('Estimated number of clusters: %d' % n_clusters_)
     print('Estimated number of noise points: %d' % n_noise_)
-    # print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    # print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    # print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    # print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
-    # print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
-    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+    #print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(pca_subset, labels))
 
 if __name__ == '__main__':
     main()
