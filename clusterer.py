24ab8826fc75203e5021259db347445fccf56e9a
Jakob Viking <joppsterr@users.noreply.github.com>
2021-03-16 10:35:17 +0100

    Updates to clusterer and gitignore

--- clusterer.py
+++ clusterer.py
@@ -5,6 +5,7 @@ from sklearn.decomposition import PCA
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from custom_functions.corr_df import corr_df
 
@@ -25,10 +26,10 @@ def main():
     # subset = dataset
     # subset = dataset[["Time","CO","Humidity","Temperature","Flow rate","Heater voltage"]]
     # subset = dataset[["Humidity","Heater voltage"]]
-    subset = corr_df(dataset, 0.7)
+    subset = corr_df(dataset, 0.45)
 
     # Reduce rows from 300 000 to specified n
-    small_subset = subset.sample(n=50000)
+    small_subset = subset.sample(n=80000)
 
     # Transform data
     scaled_subset = StandardScaler().fit_transform(small_subset)
@@ -39,6 +40,8 @@ def main():
 
     # Run DBSCAN
     db = cl.DBSCAN(eps=0.5, min_samples=5).fit(pca_subset)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
 
     # Start of results
     labels = db.labels_
@@ -48,7 +51,26 @@ def main():
 
     print('Estimated number of clusters: %d' % n_clusters_)
     print('Estimated number of noise points: %d' % n_noise_)
-    # print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(pca_subset, labels))
+    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(pca_subset, labels))
+
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = pca_subset[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
+
+        xy = pca_subset[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=0)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
 
 if __name__ == '__main__':
     main()
