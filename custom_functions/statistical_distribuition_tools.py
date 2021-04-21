5a4e508d61085726fa0157d90a8ae9640937803a
Jakob Viking <joppsterr@users.noreply.github.com>
2021-04-21 12:28:31 +0200

    final fix

--- custom_functions/statistical_distribuition_tools.py
+++ custom_functions/statistical_distribuition_tools.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import scipy
+from scipy.sparse import data
 import scipy.stats
 import matplotlib.pyplot as plt
 
@@ -8,8 +9,9 @@ from sklearn.preprocessing import StandardScaler
 from custom_functions.freedman_diaconis import freedman_diaconis
 
 
-def dist_finder(sensor_name):
-    data_set = pd.read_csv("labeled_dataset.csv")
+def dist_finder(sensor_name, dataset):
+    # data_set = pd.read_csv("labeled_dataset.csv")
+    data_set = dataset
     y = data_set[sensor_name]
 
     # Create an index array (x) for data
@@ -25,6 +27,10 @@ def dist_finder(sensor_name):
 
     # Set list of distributions to test
     # See https://docs.scipy.org/doc/scipy/reference/stats.html for more
+
+    # Turn off code warnings (this is not recommended for routine use)
+    import warnings
+    warnings.filterwarnings("ignore")
 
     dist_names = ['beta',
                 'f',
@@ -49,24 +55,12 @@ def dist_finder(sensor_name):
 
     # Set up 50 bins for chi-square test
     # Observed data will be approximately evenly distrubuted aross all bins
-    # percentile_bins = np.linspace(0,100,51)
-    # percentile_cutoffs = np.percentile(y_std, percentile_bins)
-    # observed_frequency, bins = (np.histogram(y_std, bins=percentile_cutoffs))
-    # cum_observed_frequency = np.cumsum(observed_frequency)
-
-    # Loop through candidate distributions
-
-    # Set up 50 bins for chi-square test
-        # Observed data will be approximately evenly distrubuted aross all bins
-    min = y.min()
-    max = y.max()
-        
-    # TODO: Fix percentile bins for chi square
-    percentile_bins = np.linspace(min, max, 50)
+    percentile_bins = np.linspace(0,100,51)
     percentile_cutoffs = np.percentile(y_std, percentile_bins)
     observed_frequency, bins = (np.histogram(y_std, bins=percentile_cutoffs))
     cum_observed_frequency = np.cumsum(observed_frequency)
 
+    # Loop through candidate distributions
     for distribution in dist_names:
         
         # Set up distribution and get fitted distribution parameters
@@ -101,60 +95,63 @@ def dist_finder(sensor_name):
     results['p_value'] = p_values
     results.sort_values(['chi_square'], inplace=True)
         
-    # Report results
+    return results
 
-    print ('\nDistributions sorted by goodness of fit:')
-    print ('----------------------------------------')
-    print (results)
 
-    # Divide the observed data into 100 bins for plotting (this can be changed)
-    number_of_bins = freedman_diaconis(y)
-    bin_cutoffs = np.linspace(np.percentile(y,0), np.percentile(y,99),number_of_bins)
+    # # Report results
 
-    # Create the plot
-    h = plt.hist(y, bins = bin_cutoffs, color='0.75')
+    # print ('\nDistributions sorted by goodness of fit:')
+    # print ('----------------------------------------')
+    # print (results)
 
-    # Get the top three distributions from the previous phase
-    number_distributions_to_plot = 3
-    dist_names = results['Distribution'].iloc[0:number_distributions_to_plot]
+    # # Divide the observed data into 100 bins for plotting (this can be changed)
+    # number_of_bins = freedman_diaconis(y)
+    # bin_cutoffs = np.linspace(np.percentile(y,0), np.percentile(y,99),number_of_bins)
 
-    # Create an empty list to store fitted distribution parameters
-    parameters = []
+    # # Create the plot
+    # h = plt.hist(y, bins = bin_cutoffs, color='0.75')
 
-    # Loop through the distributions ot get line fit and paraemters
+    # # Get the top three distributions from the previous phase
+    # number_distributions_to_plot = 3
+    # dist_names = results['Distribution'].iloc[0:number_distributions_to_plot]
 
-    for dist_name in dist_names:
-        # Set up distribution and store distribution paraemters
-        dist = getattr(scipy.stats, dist_name)
-        param = dist.fit(y)
-        parameters.append(param)
+    # # Create an empty list to store fitted distribution parameters
+    # parameters = []
+
+    # # Loop through the distributions ot get line fit and paraemters
+
+    # for dist_name in dist_names:
+    #     # Set up distribution and store distribution paraemters
+    #     dist = getattr(scipy.stats, dist_name)
+    #     param = dist.fit(y)
+    #     parameters.append(param)
         
-        # Get line for each distribution (and scale to match observed data)
-        pdf_fitted = dist.pdf(x, *param[:-2], loc=param[-2], scale=param[-1])
-        scale_pdf = np.trapz (h[0], h[1][:-1]) / np.trapz (pdf_fitted, x)
-        pdf_fitted *= scale_pdf
+    #     # Get line for each distribution (and scale to match observed data)
+    #     pdf_fitted = dist.pdf(x, *param[:-2], loc=param[-2], scale=param[-1])
+    #     scale_pdf = np.trapz (h[0], h[1][:-1]) / np.trapz (pdf_fitted, x)
+    #     pdf_fitted *= scale_pdf
         
-        # Add the line to the plot
-        plt.plot(pdf_fitted, label=dist_name)
+    #     # Add the line to the plot
+    #     plt.plot(pdf_fitted, label=dist_name)
         
-        # Set the plot x axis to contain 99% of the data
-        # This can be removed, but sometimes outlier data makes the plot less clear
-        plt.xlim(0,np.percentile(y,99))
+    #     # Set the plot x axis to contain 99% of the data
+    #     # This can be removed, but sometimes outlier data makes the plot less clear
+    #     plt.xlim(0,np.percentile(y,99))
 
-    # Add legend and display plot
-    plt.title(sensor_name)
-    plt.legend()
-    plt.show()
+    # # Add legend and display plot
+    # plt.title(sensor_name)
+    # plt.legend()
+    # plt.show()
 
-    # Store distribution paraemters in a dataframe (this could also be saved)
-    dist_parameters = pd.DataFrame()
-    dist_parameters['Distribution'] = (results['Distribution'].iloc[0:number_distributions_to_plot])
-    dist_parameters['Distribution parameters'] = parameters
+    # # Store distribution paraemters in a dataframe (this could also be saved)
+    # dist_parameters = pd.DataFrame()
+    # dist_parameters['Distribution'] = (results['Distribution'].iloc[0:number_distributions_to_plot])
+    # dist_parameters['Distribution parameters'] = parameters
 
-    # Print parameter results
-    print ('\nDistribution parameters:')
-    print ('------------------------')
+    # # Print parameter results
+    # print ('\nDistribution parameters:')
+    # print ('------------------------')
 
-    for index, row in dist_parameters.iterrows():
-        print ('\nDistribution:', row[0])
-        print ('Parameters:', row[1] )
+    # for index, row in dist_parameters.iterrows():
+    #     print ('\nDistribution:', row[0])
+    #     print ('Parameters:', row[1] )
--- stat_analyzer.py
+++ stat_analyzer.py
@@ -1,51 +1,31 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
-from scipy.stats import chisquare
+import sys
 
 from custom_functions.statistical_distribuition_tools import dist_finder
 
 def main():
     # Analyze statistical properties of the data
-    # sensor_data_table = pd.read_csv("datasets/gas_sensor_array.csv")
     sensor_data_table = pd.read_csv("labeled_dataset.csv")
 
     # Get amt of clusters from dataset
     num_of_clusters = sensor_data_table['Cluster'].max()
-    
-    # For each sensor in each cluster
-    #TODO: Separate each sensor into new datasets
-    for cluster, dataset_cluster in sensor_data_table.groupby('Cluster'):
-        print(dataset_cluster)
-        # if sensor_data_table.loc[sensor_data_table['Cluster'] == cluster]:
+    ite = 0
+    sys.stdout = open('output.txt','wt')
 
-    dist_finder("Temperature")
-    dist_finder("Humidity")
+    while ite < num_of_clusters:
 
-    # for sensor_name in list(sensor_data_table.columns):
-        # dist_finder(sensor_name)
-
-    # Separate each sensor and plot
-    # for sensor_name in list(sensor_data_table.columns):
-        # Calc suggested number of bins in histogram based on Freedman-Diaconis rule
-        # amt_of_bins = freedman_diaconis(sensor_data_table[sensor_name])
-
-
-        # Compare to other probability distribution
-        # for prob_dist in dists:
-        #     chi2 = chisquare(sensor_data_table[sensor_name], prob_dist)
-        #     print("Chi square value %d", chi2)
-    
-        # Save probability distribution, max value, min value
-
-
-    # Output to json file with states
-    # or as each unique value in sensor
-    # amt_of_bins = np.arange(sensor_data_table[sensor_name].min(), sensor_data_table[sensor_name].max())
-    
-    # plt.hist(sensor_data_table[sensor_name], amt_of_bins)
-    # plt.title(sensor_name)
-    # plt.show()
+        grouped_by_cluster = sensor_data_table.groupby(sensor_data_table['Cluster'])
+        data_one_cluster = grouped_by_cluster.get_group(ite)
+        for sensor_name in list(sensor_data_table.columns):
+            df = dist_finder(sensor_name,data_one_cluster)
+            print("state: "+ str(ite))
+            print(sensor_name)
+            print(df['Distribution'].iloc[0])
+            print(df['chi_square'].iloc[0])
+            print(df['p_value'].iloc[0])
+        ite = ite + 1
+ 
+        
 
 if __name__ == '__main__':
     main()
